6a5f6b57274a1f21d3723b1fa6a2ec7a4fb0e7a5
christiantepora <teporacj@gmail.com>
2021-06-21 12:08:44 +0800

    add check and trash functionality

--- todo-list/app.js
+++ todo-list/app.js
@@ -4,6 +4,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 //event listener
 todoButton.addEventListener("click", addTodo)
+todoList.addEventListener("click", deleteCheck)
 //functions
 function addTodo(event){
     event.preventDefault();
@@ -35,4 +36,20 @@ function addTodo(event){
     //clear
 
     todoInput.value="";
+}
+
+function deleteCheck(e){
+const item = e.target;
+
+
+if(item.classList[0]==='trash-btn'){
+    const todo=item.parentElement;
+todo.remove();
+}
+if(item.classList[0]==="complete-btn"){
+    const todo=item.parentElement;
+    todo.classList.toggle("completed");
+
+}
+
 }
